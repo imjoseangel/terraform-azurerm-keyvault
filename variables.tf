3223307cab3a6e351bd1084c86db33d81026b1b9
imjoseangel <josea.munoz@gmail.com>
2021-12-17 08:29:50 +0100

    rsg to false

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "name" {
   description = "Name of key vault account."
+  type        = string
 }
 
 variable "create_resource_group" {
   description = "Whether to create resource group and use it for all networking resources"
-  default     = true
+  type        = bool
+  default     = false
 }
 
 variable "resource_group_name" {
   description = "A container that holds related resources for an Azure solution"
+  type        = string
   default     = "rg-demo-westeurope-01"
 }
 
 variable "location" {
   description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  type        = string
   default     = "westeurope"
 }
 
 variable "skuname" {
   description = "The SKUs supported by Microsoft Key Vault. Valid options are premium, standard"
+  type        = string
   default     = "standard"
 }
 
